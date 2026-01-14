241593a6f8775efdec646c5c9035f87beeb9ee2e
tabish <tabishimam609@outlook.com>
2026-01-14 20:36:04 +0530

    terraform

--- Terraform/Application/main.tf
+++ Terraform/Application/main.tf
@@ -2,7 +2,7 @@ module "ec2" {
     source         = "./module"
     instance_type  = var.instance_type
     ami            = var.ami
-    security_group =  = var.security_group
+    security_group = var.security_group
     subnet_id      = var.subnet_id
     
 }
