ede9d700a28e60a3bd460a5b081598a2ab4ef059
tabish <tabishimam609@outlook.com>
2026-01-14 19:20:27 +0530

    terraform

--- Terraform/Application/module/main.tf
+++ Terraform/Application/module/main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "instance" {
+resource "aws_instance" "example" {
     instance_type = var.instance_type
     ami           = var.ami
     subnet_id     = var.subnet_id
-    vpc_security_group_ids = [var.security_group]
+    security_groups = [var.security_group]
     
     tags = {
       Name = "TerraformExampleInstance"
